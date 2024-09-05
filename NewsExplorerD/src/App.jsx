476ab17d64005ea13c1f65ffbbe9f5ae47829fea
Rafael <raas@cin.ufpe.br>
2024-09-05 17:26:32 -0300

    Criação do proejto com Vite

---
+++ NewsExplorerD/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { Header } from './components/Header'
+import NewsList from './services/NewsList'; // Import the NewsList component
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <main>
+        <NewsList />
+      </main>
+    </>
+  )
+}
+
+export default App
